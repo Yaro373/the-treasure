dc0991b8162645f9fc874dda12e0f6e7d6e63961
Yaro373 <zakoyasse@gmail.com>
2021-12-27 11:05:14 +0300

    initial commit

--- view/wall.py
+++ view/wall.py
@@ -27,3 +27,14 @@ class Wall2(Wall):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+
+class Wall3(Wall):
+    image = load_image('wall_3')
+
+    def __init__(self, x, y, *group):
+        super().__init__(*group)
+        self.image = Wall1.image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
