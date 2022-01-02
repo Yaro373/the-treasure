410e1048a4b049c5004ca56c55f957ce6ff5f6ae
banana-dan <79079738+banana-dan@users.noreply.github.com>
2022-01-02 16:43:53 +0300

    - dungeon generation script changed - UnbreakableWall class added to object_sprites.py

--- view/dungeon.py
+++ view/dungeon.py
@@ -1,13 +1,15 @@
 import pygame
 import random
 from view.object_sprites import Floor1
-from view.object_sprites import Wall1
+from view.object_sprites import Wall1, UnbreakableWall
 import view.creature
 from parameters import CELL_SIZE
 
 NOTHING_SIGN = 0
 WALL_SIGN = 1
 GHOST_SIGN = 2
+CHEST_SIGN = 3
+UNBREAKABLE_WALL_SIGN = 4
 
 
 class Dungeon:
@@ -26,6 +28,9 @@ class Dungeon:
                 if self.data[row][col] == WALL_SIGN:
                     Wall1(row * CELL_SIZE, col * CELL_SIZE, self.walls_sprite_group,
                           self.all_sprites)
+                elif self.data[row][col] == UNBREAKABLE_WALL_SIGN:
+                    UnbreakableWall(row * CELL_SIZE, col * CELL_SIZE, self.walls_sprite_group,
+                                    self.all_sprites)
                 else:
                     Floor1(row * CELL_SIZE, col * CELL_SIZE, self.floor_sprite_group,
                            self.all_sprites)
@@ -54,86 +59,70 @@ class Dungeon:
 
 class DungeonGenerator:
     @staticmethod
-    def randomize_ways(data, row, col):
-        row_pos_del = 0
-        col_pos_del = 0
-        result = [1, 2, 3, 4]  # 1 - вверх 2 - вправо 3 - вниз 4 - влево
-        # когда переменная равна 1, значит по координате позиция находится в максимальном положении
-        # -1 - в минимальном
-        if row == 0:
-            row_pos_del = -1
-            result.remove(1)
-        elif row == len(data) - 1:
-            row_pos_del = 1
-            result.remove(3)
-        if col == 0:
-            col_pos_del = -1
-            result.remove(4)
-        elif col == len(data) - 1:
-            col_pos_del = 1
-            result.remove(2)
+    def generate(n):
+        stek = [(1, 1)]  # список посещенных клеток
+        result = [[WALL_SIGN for i in range(n * 2 + 1)] for i in
+                  range(n * 2 + 1)]  # подземелье в виде матрицы
+        unvisited_cells_num = n * n - 1  # количество непосещенных клеток
+        visited = [(1, 1)]  # список со всеми посещенными клетками
+        current_cell = 1, 1  # текущие координаты
+        result[1][1] = NOTHING_SIGN
+        # окружаем поле неразрушимыми стенами
+        for i in range(len(result)):
+            if i == 0 or i == len(result) - 1:
+                for j in range(len(result)):
+                    result[i][j] = UNBREAKABLE_WALL_SIGN
+            else:
+                result[i][0] = UNBREAKABLE_WALL_SIGN
+                result[i][-1] = UNBREAKABLE_WALL_SIGN
+        # прокладываем дороги
+        while unvisited_cells_num > 0:
+            # проверяем, есть ли не посещенные соседи
+            unvisited_cells = []
+            # слева 1
+            if current_cell[1] > 1 and (current_cell[0], current_cell[1] - 2) not in visited:
+                unvisited_cells.append(1)
+            # вверх 2
+            if current_cell[0] > 1 and (current_cell[0] - 2, current_cell[1]) not in visited:
+                unvisited_cells.append(2)
+            # справа 3
+            if current_cell[1] < len(result) - 2 and \
+                    (current_cell[0], current_cell[1] + 2) not in visited:
+                unvisited_cells.append(3)
+            # снизу 4
+            if current_cell[0] < len(result) - 2 and \
+                    (current_cell[0] + 2, current_cell[1]) not in visited:
+                unvisited_cells.append(4)
+            # если у точки есть непосещенные соседи
 
-        # проверяем верх (1)
-        if row_pos_del != -1:
-            if data[row - 2][col] == NOTHING_SIGN:
-                result.remove(1)
-        # проверяем низ (3)
-        if row_pos_del != 1:
-            if data[row + 2][col] == NOTHING_SIGN:
-                result.remove(3)
-        # проверяем право (2)
-        if col_pos_del != 1:
-            if data[row][col + 2] == NOTHING_SIGN:
-                result.remove(2)
-        # проверяем лево (4)
-        if col_pos_del != -1:
-            if data[row][col - 2] == NOTHING_SIGN:
-                result.remove(4)
-        if len(result) == 0:
-            rcresult = 0
-        else:
-            rcresult = random.choice(result)
+            if len(unvisited_cells) > 0:
+                # перемещяемся
+                choise = random.choice(unvisited_cells)
+                stek.append(current_cell)
+                if choise == 1:  # лево
+                    result[current_cell[0]][current_cell[1] - 2] = NOTHING_SIGN
+                    result[current_cell[0]][current_cell[1] - 1] = NOTHING_SIGN
+                    current_cell = current_cell[0], current_cell[1] - 2
+                if choise == 2:  # верх
+                    result[current_cell[0] - 1][current_cell[1]] = NOTHING_SIGN
+                    result[current_cell[0] - 2][current_cell[1]] = NOTHING_SIGN
+                    current_cell = current_cell[0] - 2, current_cell[1]
+                if choise == 3:  # справа
+                    result[current_cell[0]][current_cell[1] + 2] = NOTHING_SIGN
+                    result[current_cell[0]][current_cell[1] + 1] = NOTHING_SIGN
+                    current_cell = current_cell[0], current_cell[1] + 2
+                if choise == 4:  # снизу
+                    result[current_cell[0] + 1][current_cell[1]] = NOTHING_SIGN
+                    result[current_cell[0] + 2][current_cell[1]] = NOTHING_SIGN
+                    current_cell = current_cell[0] + 2, current_cell[1]
+            # иначе
+            else:
+                current_cell = stek.pop(-1)
+            if current_cell not in visited:
+                visited.append(current_cell)
+                unvisited_cells_num -= 1
 
-        if rcresult == 0:
-            return 0, 0
-        if rcresult == 1:
-            return -1, 0
-        if rcresult == 2:
-            return 0, 1
-        if rcresult == 3:
-            return 1, 0
-        if rcresult == 4:
-            return 0, -1
-
-    @staticmethod
-    def generate(dots_num, is_first=True, row=0, col=0, pos_data=None, data=None):
-        if data is None:
-            data = []
-        if pos_data is None:
-            pos_data = []
-
-        if is_first:
-            is_first = False
-            data = [[WALL_SIGN for i in range(dots_num * 2 - 1)] for j in range(dots_num * 2 - 1)]
-            dots_num = dots_num * dots_num - 1
-            data[row][col] = NOTHING_SIGN
-            pos_data.append((row, col))
-            return DungeonGenerator.generate(dots_num, is_first, row, col, pos_data, data)
-        elif dots_num == 0:
-            return data
-        else:
-            rand = DungeonGenerator.randomize_ways(data, row, col)
-            if rand == (0, 0):
-                # back step
-                cords = pos_data.pop(-1)
-                return DungeonGenerator.generate(dots_num, is_first, cords[0], cords[1], pos_data, data)
-            data[row + rand[0]][col + rand[1]] = NOTHING_SIGN
-            row = row + rand[0] * 2
-            col = col + rand[1] * 2
-            data[row][col] = NOTHING_SIGN
-            dots_num -= 1
-            pos_data.append((row, col))
-            return DungeonGenerator.generate(dots_num, is_first, row, col, pos_data, data)
+        return result
 
 
 class Camera:
--- view/object_sprites.py
+++ view/object_sprites.py
@@ -32,6 +32,13 @@ class Wall3(BaseObjectSprite):
         super().__init__(Wall3.image, x, y, *group)
 
 
+class UnbreakableWall(BaseObjectSprite):
+    image = load_image("64x64_wall_3.png")  # todo поменять спрайт
+
+    def __init__(self, x, y, *group):
+        super().__init__(Wall3.image, x, y, *group)
+
+
 class Floor1(BaseObjectSprite):
     image = load_image('64x64_floor_1.png')
 
@@ -74,4 +81,3 @@ class Trap(BaseObjectSprite):
 
     def __init__(self, x, y, *group):
         super().__init__(Chest.opened_chest_image, x, y, *group)
-
